7d8ca5f0972ca2759b11686947db75dd672f8d09
Erace17 <73711284+Erace17@users.noreply.github.com>
2022-10-17 17:31:37 +0200

    edited caption

--- Temperatur.py
+++ Temperatur.py
@@ -9,7 +9,7 @@ def cel():
     celsius = kelvin - 273.15
     if kelvin < 0:
         print(kelvin, "Grad Kelvin, sind", celsius, "Grad Celsius.")
-        print("Achtung!:", celsius, "Grad Celsius /", kelvin, "Grad Kelvin, sind physikalisch nicht zu erreichende Temperaturen!")
+        print("Achtung!:", kelvin, "Grad Kelvin /", celsius, "Grad Celsius, sind physikalisch nicht zu erreichende Temperaturen!")
     else:
         print(kelvin, "Grad Kelvin, sind", celsius, "Grad Celsius.")
 
